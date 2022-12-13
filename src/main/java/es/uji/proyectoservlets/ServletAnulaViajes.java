2dc932ac2e95cd9b2fdd03ac632f79e5a5862d13
BorjaPQ <borjaperezquinonero@hotmail.com>
2022-12-13 12:10:41 +0100

    All html

---
+++ src/main/java/es/uji/proyectoservlets/ServletAnulaViajes.java
@@ -0,0 +1,20 @@
+package es.uji.proyectoservlets;
+
+import jakarta.servlet.*;
+import jakarta.servlet.http.*;
+import jakarta.servlet.annotation.*;
+
+import java.io.IOException;
+
+@WebServlet(name = "ServletAnulaViajes", value = "/ServletAnulaViajes")
+public class ServletAnulaViajes extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+    }
+}
